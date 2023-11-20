19151625d3aafaaa100553338c7dea9a88d9eece
mmussett <mmussett@me.com>
2023-11-20 16:26:19 +0000

    First commit

---
+++ extensions/gcp/activity/getidtoken/activity_test.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2017. TIBCO Software Inc.
+ * This file is subject to the license terms contained
+ * in the license file that is distributed with this file.
+ */
+package getidtoken
+
+import (
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+	return activityMetadata
+}
+
+func TestActivityRegistration(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	if act == nil {
+		t.Error("Activity Not Registered")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(act.Metadata())
+	//setup attrs
+	tc.SetInput("url", "#")
+	_, err := act.Eval(tc)
+	assert.Nil(t, err)
+	accessToken := tc.GetOutput("accessToken")
+	assert.Empty(t, accessToken)
+
+}
